2669efd107a415b7d11d7b1801207f2de04cd856
James <jammesliu@hotmail.com>
2017-12-12 14:58:25 +0800

    update product service port

--- product-service/src/main/java/com/bailian/service/product/controller/UserController.java
+++ product-service/src/main/java/com/bailian/service/product/controller/UserController.java
@@ -4,6 +4,8 @@ import com.bailian.service.product.model.User;
 import com.bailian.service.product.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +16,24 @@ import java.util.List;
 @CrossOrigin
 public class UserController {
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @RequestMapping
     public List<User> getAll() {
+        logger.info("开始查询所有用户......");
         List<User> users = userService.getAll();
+        logger.info("结束查询所有用户......");
         return users;
     }
 
     @RequestMapping("/{id}")
     public User getById(@PathVariable Integer id) {
+        logger.info("开始查询" + id +"用户......");
         User user = userService.getById(id);
+        logger.info("结束查询" + id +"用户......");
         System.out.println(user.toString());
         return user;
     }
@@ -35,12 +43,15 @@ public class UserController {
     @ApiOperation(value="create a user ", notes="new user")
     @ApiImplicitParam(name = "user", value = "userBean", required = true, dataType = "User")
     public User saveOrUpdate(@RequestBody User user) {
+        logger.info("开始更新" + user.getUsername() +"用户......");
         userService.saveOrUpdate(user);
+        logger.info("结束更新" + user.getUsername() +"用户......");
         return user;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public int deleteById(@PathVariable Integer id) {
+        logger.info("开始删除" + id +"用户......");
         return userService.deleteById(id);
     }
 }
